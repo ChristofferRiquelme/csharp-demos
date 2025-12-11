7bc54febd5401bb325983bfa18983aca1b1f3a95
ChristofferRiquelme <christoffer.riquelme@gmail.com>
2025-12-11 16:12:55 +0100

    Initial commit

---
+++ ConsoleApp.SampleCalculator/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Welcome message
+Console.WriteLine("********** - Welcome to the sample calculator! - **********");
+
+
+// Show calculator options / Show menu
+Console.WriteLine("Please select an operation");
+Console.WriteLine("1. Addition");
+Console.WriteLine("2. Subtraction");
+Console.WriteLine("3. Multiplication");
+Console.WriteLine("4. Division");
+int choice = Convert.ToInt32(Console.ReadLine());
+
+// Prompt for user input
+Console.Write("Please enter the first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Please enter the second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+// Decide which operation is needed based on selected option
+int answer = 0;
+switch (choice)
+{
+    case 1:
+        answer = num1 + num2;
+        break;
+    case 2:
+        answer = num1 - num2;
+        break;
+    case 3:
+        answer = num1 * num2;
+        break;
+    case 4:
+        answer = num1 / num2;
+        break;
+    default:
+        Console.WriteLine("Invalid choice");
+        break;
+}
+
+// Print output
+Console.WriteLine($"The result is {answer}");
+Console.WriteLine("Thank you for using the sample calculator!");
